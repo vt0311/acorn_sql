385e7e25b49a5d6f57788bbe80ab515f9582060a
swhah <hsw0311@nate.com>
2017-12-12 17:40:10 +0900

    1

--- MainDB1.sql
+++ MainDB1.sql
@@ -1,10 +1,15 @@
 alter table drug_2014 add atc_code varchar2(100);
 select * from drug_2014;
+
 --순환개선제 (Circulatory Improvement Agents), Ginkgo biloba, ginkgo biloba ext. 80mg
 update drug_2014 set atc_code = 'N06DX02' where drug_ingredient_code='165305ATB';
+select * from drug_2014 where drug_ingredient_code='165305ATB';
+commit;
 
 -- amitriptyline HCl 10mg
 update drug_2014 set atc_code = 'N06AA09' where drug_ingredient_code='107501ATB';
+select * from drug_2014 where drug_ingredient_code='107501ATB';
+commit;
 
 update drug_2014 set atc_code = 'N06DX02' where drug_ingredient_code='165305ATB';
 
